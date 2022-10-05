67aee680d49f0061d69f69f0039893d4152e6638
Bas van Dinther <bas@vormkracht10.nl>
2022-10-05 16:19:09 +0200

    Add Illuminate HTTP package to make API calls for authorization

--- src/Genesys.php
+++ src/Genesys.php
@@ -2,6 +2,8 @@
 
 namespace Vormkracht10\GenesysApi;
 
+use Illuminate\Support\Facades\Http;
+
 class Genesys
 {
     private $clientId; 
@@ -14,7 +16,7 @@ class Genesys
     
     private $redirectUrl;
 
-    private $client;
+    private $loginUrl;
 
     private $region;
 
@@ -73,6 +75,16 @@ class Genesys
         return $this->redirectUrl;
     }
 
+    public function setLoginUrl(string $loginUrl): void
+    {
+        $this->loginUrl = $loginUrl;
+    }
+
+    public function getLoginUrl(): string
+    {
+        return $this->loginUrl;
+    }
+
     public function setRegion(string $region): void
     {
         $this->region = $region;
@@ -96,8 +108,56 @@ class Genesys
         return 'https://login.mypurecloud.com' . '/oauth/authorize?' . $query;
     }
 
-    public function requestAccessToken($code)
+    public function requestAccessToken(string $code): string
     {
-        // 
+        $response = Http::asForm()
+            ->withBasicAuth(
+                $this->getClientId(),
+                $this->getClientSecret()
+            )->post($this->getLoginUrl() . '/oauth/token', [
+                'grant_type' => 'authorization_code',
+                'code' => $code,
+                'redirect_uri' => $this->getRedirectUrl(),
+            ])
+            ->throw()
+            ->json();
+
+        $this->setAccessToken($response['access_token']);
+
+        return $this->getAccessToken();
+    }
+
+    public function requestAccesTokenWithRefreshToken(string $refreshToken): string
+    {
+        $response = Http::asForm()
+            ->withBasicAuth(
+                $this->getClientId(),
+                $this->getClientSecret()
+            )->post($this->getLoginUrl() . '/oauth/token', [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $refreshToken,
+            ])
+            ->throw()
+            ->json();
+
+        $this->setAccessToken($response['access_token']);
+
+        return $this->getAccessToken();
+    }
+
+    /** @todo not sure if we need this, need confirmation. */
+    public function requestApiToken()
+    {
+        $response = Http::asForm()
+            ->post($this->getLoginUrl() . '/oauth/token', [
+                'grant_type' => 'client_credentials',
+                'client_id' => $this->getClientId(),
+                'client_secret' => $this->getClientSecret(),
+                'scope' => '',
+            ])
+            ->throw()
+            ->json();
+
+        $this->setAccessToken($response['access_token']);
     }
 }
