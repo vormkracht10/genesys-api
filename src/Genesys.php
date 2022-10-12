0249aecbf4f0007f811ae1e8c810aa43353d5fee
Bas van Dinther <bas@vormkracht10.nl>
2022-10-12 09:53:34 +0200

    Remove authorization

--- src/Genesys.php
+++ src/Genesys.php
@@ -6,212 +6,101 @@ use GuzzleHttp\Client;
 
 class Genesys
 {
-    private string $clientId;
-
-    private string $clientSecret;
-
-    private string $accessToken;
-
-    private string $refreshToken;
-
-    private string $redirectUrl;
+    public string $accessToken;
 
     public string $region;
 
-    private string $appUrl = 'https://api.mypurecloud.';
+    public string $apiDomain = 'https://api.mypurecloud.com/api/v2';
 
-    private string $loginUrl = 'https://login.mypurecloud.';
+    public string $authDomain = 'https://login.mypurecloud.com';
 
-    private string $apiUrl = 'https://api.mypurecloud.';
+    public Client $client;
 
-    private Client $client;
-
-    public function __construct(string $region)
+    public function __construct(string $accessToken, string $region)
     {
+        $this->accessToken = $accessToken;
         $this->region = $region;
+    
         $this->client = new Client();
 
-        $this->createUrlsForRegion($region);
+        $this->setRegion($region);
     }
 
-    public static function api(string $region): Genesys
+    public static function api(string $accessToken, string|null $region = null): Genesys
     {
-        return new Genesys($region);
+        return new Genesys(accessToken: $accessToken, region: $region);
     }
 
-    private function createUrlsForRegion(string $region): void
+  /**
+     * Set the correct API and Auth domains based on the region
+     * @see https://developer.genesys.cloud/platform/api/
+     *
+     * @param string $region
+     * @return void
+     */
+    private function setRegion(string $region): void
     {
         switch ($region) {
-            case 'eu':
-                $this->setUrls($this->appUrl . 'eu', $this->loginUrl . 'eu', $this->apiUrl . 'eu');
+            case 'us-east-1':
+                $this->apiDomain = 'https://api.mypurecloud.com/api/v2';
+                $this->authDomain = 'https://login.mypurecloud.com';
 
                 break;
-
-            case 'jp':
-                $this->setUrls($this->appUrl . 'jp', $this->loginUrl . 'jp', $this->apiUrl . 'jp');
+            case 'us-east-2':
+                $this->apiDomain = 'https://api.use2.us-gov-pure.cloud/api/v2';
+                $this->authDomain = 'https://login.use2.us-gov-pure.cloud';
 
                 break;
+            case 'us-west-2':
+                $this->apiDomain = 'https://api.usw2.pure.cloud/api/v2';
+                $this->authDomain = 'https://login.usw2.pure.cloud';
 
-            case 'us':
-            default:
-                $this->setUrls($this->appUrl . 'com', $this->loginUrl . 'com', $this->apiUrl . 'com');
+                break;
+            case 'ca-central-1':
+                $this->apiDomain = 'https://api.cac1.pure.cloud/api/v2';
+                $this->authDomain = 'https://login.cac1.pure.cloud';
+
+                break;
+            case 'sa-east-1':
+                $this->apiDomain = 'https://api.sae1.pure.cloud/api/v2';
+                $this->authDomain = 'https://login.sae1.pure.cloud';
+
+                break;
+            case 'eu-central-1':
+                $this->apiDomain = 'https://api.mypurecloud.de/api/v2';
+                $this->authDomain = 'https://login.mypurecloud.de';
+
+                break;
+            case 'eu-west-1':
+                $this->apiDomain = 'https://api.mypurecloud.ie/api/v2';
+                $this->authDomain = 'https://login.mypurecloud.ie';
+
+                break;
+            case 'eu-west-2':
+                $this->apiDomain = 'https://api.euw2.pure.cloud/api/v2';
+                $this->authDomain = 'https://login.euw2.pure.cloud';
+
+                break;
+            case 'ap-south-1':
+                $this->apiDomain = 'https://api.aps1.pure.cloud/api/v2';
+                $this->authDomain = 'https://login.aps1.pure.cloud';
+
+                break;
+            case 'ap-northeast-2':
+                $this->apiDomain = 'https://api.apne2.pure.cloud/api/v2';
+                $this->authDomain = 'https://login.apne2.pure.cloud';
+
+                break;
+            case 'ap-southeast-2':
+                $this->apiDomain = 'https://api.mypurecloud.com.au/api/v2';
+                $this->authDomain = 'https://login.mypurecloud.com.au';
+
+                break;
+            case 'ap-northeast-1':
+                $this->apiDomain = 'https://api.mypurecloud.jp/api/v2';
+                $this->authDomain = 'https://login.mypurecloud.jp';
 
                 break;
         }
-    }
-
-    private function setUrls(string $appUrl, string $loginUrl, string $apiUrl): void
-    {
-        $this->appUrl = $appUrl;
-        $this->loginUrl = $loginUrl;
-        $this->apiUrl = $apiUrl . '/api/v2';
-    }
-
-    public function setClientId(string $clientId): self
-    {
-        $this->clientId = $clientId;
-
-        return $this;
-    }
-
-    public function getClientId(): string
-    {
-        return $this->clientId;
-    }
-
-    public function setClientSecret(string $clientSecret): self
-    {
-        $this->clientSecret = $clientSecret;
-
-        return $this;
-    }
-
-    public function getClientSecret(): string
-    {
-        return $this->clientSecret;
-    }
-
-    public function setAccessToken(string $accessToken): self
-    {
-        $this->accessToken = $accessToken;
-
-        return $this;
-    }
-
-    public function getAccessToken(): string
-    {
-        return $this->accessToken;
-    }
-
-    public function setRefreshToken(string $refreshToken): self
-    {
-        $this->refreshToken = $refreshToken;
-
-        return $this;
-    }
-
-    public function getRefreshToken(): string
-    {
-        return $this->refreshToken;
-    }
-
-    public function setRedirectUrl(string $redirectUrl): self
-    {
-        $this->redirectUrl = $redirectUrl;
-
-        return $this;
-    }
-
-    public function getRedirectUrl(): string
-    {
-        return $this->redirectUrl;
-    }
-
-    public function setLoginUrl(string $loginUrl): self
-    {
-        $this->loginUrl = $loginUrl;
-
-        return $this;
-    }
-
-    public function getLoginUrl(): string
-    {
-        return $this->loginUrl;
-    }
-
-    public function getAuthorizationUrl(): string
-    {
-        $query = http_build_query([
-            'client_id' => $this->getClientId(),
-            'redirect_uri' => $this->getRedirectUrl(),
-            'response_type' => 'code',
-            'scope' => '',
-        ]);
-
-        return $this->getLoginUrl() . '/oauth/authorize?' . $query;
-    }
-
-    public function requestAccessToken(string $code): array
-    {
-        $response = $this->client->post($this->getLoginUrl() . '/oauth/token', [
-            'auth' => [
-                $this->getClientId(),
-                $this->getClientSecret(),
-            ],
-            'grant_type' => 'authorization_code',
-            'code' => $code,
-            'redirect_uri' => $this->getRedirectUrl(),
-        ])
-        ->getBody()
-        ->getContents();
-
-        $response = json_decode($response, true);
-
-        $this->setAccessToken($response['access_token']);
-        $this->setRefreshToken($response['refresh_token']);
-
-        return $response;
-    }
-
-    public function requestAccessTokenWithRefreshToken(string $refreshToken): array
-    {
-        $response = $this->client->post($this->getLoginUrl() . '/oauth/token', [
-            'auth' => [
-                $this->getClientId(),
-                $this->getClientSecret(),
-            ],
-            'form_params' => [
-                'grant_type' => 'refresh_token',
-                'refresh_token' => $refreshToken,
-            ],
-        ])
-        ->getBody()
-        ->getContents();
-
-        $response = json_decode($response, true);
-
-        $this->setAccessToken($response['access_token']);
-
-        return $response;
-    }
-
-    public function requestApiToken(): array
-    {
-        $response = $this->client->post($this->getLoginUrl() . '/oauth/token', [
-            'form_params' => [
-                'grant_type' => 'client_credentials',
-                'client_id' => $this->getClientId(),
-                'client_secret' => $this->getClientSecret(),
-                'scope' => '',
-            ],
-        ])
-        ->getBody()
-        ->getContents();
-
-        $response = json_decode($response, true);
-
-        $this->setAccessToken($response['access_token']);
-
-        return $response;
     }
 }
