687b23e94490127197d212a68e0eebbb20c39e11
Bas van Dinther <bas@vormkracht10.nl>
2022-10-17 11:07:34 +0200

    Add service provider and config file

---
+++ src/ServiceProvider/Genesys.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace Vormkracht10\GenesysApi\ServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+use Vormkracht10\GenesysApi\Genesys as GenesysApi;
+
+class Genesys extends ServiceProvider 
+{
+    public function register(): void
+    {
+        $this->app->singleton(GenesysApi::class, function ($app) {
+
+            $config = $app->make('config')->get('genesys');
+
+            return new GenesysApi(
+                region: $config['region'],
+                accessToken: $config['access_token'],
+                // Get access token by user
+            );
+        });
+
+        // $this->app->when(GenesysApi::class)
+        //     ->needs('$accessToken')
+        //     ->give($accessToken);
+    }
+
+    /** @return array<int, string> */
+    public function provides(): array
+    {
+        return [GenesysApi::class];
+    }
+}
---
+++ src/config/genesys.php
@@ -0,0 +1,5 @@
+<?php 
+
+return [
+    'region' => 'us-east-1',
+];
